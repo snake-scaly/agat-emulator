41cad60d6328cb5438850b585aafdb9ee2347349
Nop <nnop@newmail.ru>
2010-02-19 08:10:12 +0000

    Added cfgedit auto building support

--- src/makeall.bat
+++ src/makeall.bat
@@ -1,5 +1,5 @@
 @echo off
-for %%n in (cpu fdd joystick sound tape video installio lang langsel softcard clock printer ramcard .) do (
+for %%n in (cpu fdd joystick sound tape video installio lang langsel softcard clock printer ramcard cfgedit .) do (
 	echo Building %%n...
 	pushd %%n
 	call make.bat
