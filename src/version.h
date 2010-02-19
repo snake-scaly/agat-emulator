c8259e3484e67b379cf442d1dd547c08cc6b8c88
Nop <nnop@newmail.ru>
2010-02-19 08:08:19 +0000

    Version 1.12: added some sound corrections; added writing support for AIMs

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
-#define VERSION_NAME_STR	"Agat v1.11"
-#define VERSION_STR	"1.11"
-#define VERSION_VER	1, 11, 0, 0
-#define VERSION_VER_STR	"1, 11, 0, 0\0"
+#define VERSION_NAME_STR	"Agat v1.12"
+#define VERSION_STR	"1.12"
+#define VERSION_VER	1, 12, 0, 0
+#define VERSION_VER_STR	"1, 12, 0, 0\0"
 
