40571b096e68e41ab448b349e7fc635d458804d1
Nop <nnop@newmail.ru>
2010-03-06 14:25:48 +0000

    Release 1.12.1

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
-#define VERSION_NAME_STR	"Agat v1.12"
-#define VERSION_STR	"1.12"
-#define VERSION_VER	1, 12, 0, 0
-#define VERSION_VER_STR	"1, 12, 0, 0\0"
+#define VERSION_NAME_STR	"Agat v1.12.1"
+#define VERSION_STR	"1.12.1"
+#define VERSION_VER	1, 12, 1, 0
+#define VERSION_VER_STR	"1, 12, 1, 0\0"
 
