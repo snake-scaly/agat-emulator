1ea62f7ab80362882e54e15309e57a5cfd4c1d48
Nop <nnop@newmail.ru>
2010-12-12 21:48:55 +0000

    Version 1.15 has been released

--- src/makeall.bat
+++ src/makeall.bat
@@ -1,6 +1,5 @@
 @echo off
 call "F:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat" 
-cl
 for %%n in (cpu fdd joystick sound tape video installio lang langsel softcard clock printer mouse ramcard cfgedit .) do (
 	echo Building %%n...
 	pushd %%n
--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
-#define VERSION_NAME_STR	"Agat v1.14"
-#define VERSION_STR	"1.14"
-#define VERSION_VER	1, 14, 0, 0
-#define VERSION_VER_STR	"1, 14, 0, 0\0"
+#define VERSION_NAME_STR	"Agat v1.15"
+#define VERSION_STR	"1.15"
+#define VERSION_VER	1, 15, 0, 0
+#define VERSION_VER_STR	"1, 15, 0, 0\0"
 
