ecff3f26b24b992d4e936f1ba66866dda938d09d
Nop <nnop@newmail.ru>
2011-01-21 18:56:11 +0000

    Version 1.16

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
-#define VERSION_NAME_STR	"Agat v1.15"
-#define VERSION_STR	"1.15"
-#define VERSION_VER	1, 15, 0, 0
-#define VERSION_VER_STR	"1, 15, 0, 0\0"
+#define VERSION_NAME_STR	"Agat v1.16"
+#define VERSION_STR	"1.16"
+#define VERSION_VER	1, 16, 0, 0
+#define VERSION_VER_STR	"1, 16, 0, 0\0"
 
