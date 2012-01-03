1bc0e52a1f4f183a268a6df7054086352fee2225
Nop <nnop@newmail.ru>
2012-01-03 18:20:01 +0000

    New version

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
-#define VERSION_NAME_STR	"Agat v1.21"
-#define VERSION_STR	"1.21"
-#define VERSION_VER	1, 21, 0, 0
-#define VERSION_VER_STR	"1, 21, 0, 0\0"
+#define VERSION_NAME_STR	"Agat v1.22"
+#define VERSION_STR	"1.22"
+#define VERSION_VER	1, 22, 0, 0
+#define VERSION_VER_STR	"1, 22, 0, 0\0"
 
