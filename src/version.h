61c3747adbd4b713f299c51c4d0add83500e0343
SnakE <snake.scaly@gmail.com>
2017-01-16 10:53:53 +0000

    Bump emulator version to 1.28

--- src/version.h
+++ src/version.h
@@ -1,5 +1,7 @@
-#define VERSION_NAME_STR	"Agat v1.27"
-#define VERSION_STR	"1.27"
-#define VERSION_VER	1, 27, 0, 0
-#define VERSION_VER_STR	"1, 27, 0, 0\0"
-
+#define VERSION_NAME_STR	"Agat v1.28"
+#define VERSION_STR	"1.28"
+#define VERSION_VER	1, 28, 0, 0
+#define VERSION_VER_STR	"1.28.0.0"
+#define VERSION_DESC_STR "Agat-7,9, Apple ][ emulator"
+#define VERSION_AUTH_STR "Nop"
+#define VERSION_COPY_STR "Copyright © 2008-2016 Nop"
