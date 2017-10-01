75abf221a0daf9fb107b9114477dad344350c1bc
Nop <nnop@newmail.ru>
2017-10-01 19:30:48 +0000

    Release 1.28

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
-#define VERSION_NAME_STR	"Agat v1.28"
-#define VERSION_STR	"1.28"
-#define VERSION_VER	1, 28, 0, 0
-#define VERSION_VER_STR	"1.28.0.0"
+#define VERSION_NAME_STR	"Agat v1.29"
+#define VERSION_STR	"1.29"
+#define VERSION_VER	1, 29, 0, 0
+#define VERSION_VER_STR	"1.29.0.0"
 #define VERSION_DESC_STR "Agat-7,9, Apple ][ emulator"
-#define VERSION_AUTH_STR "Nop"
-#define VERSION_COPY_STR "Copyright © 2008-2016 Nop"
+#define VERSION_AUTH_STR "Nop & SnakE"
+#define VERSION_COPY_STR "Copyright © 2008-2017 Nop, SnakE"
