c5566f1be0f0012e271885692109325ac63bf854
SnakE <snake.scaly@gmail.com>
2019-04-28 16:58:22 +0000

    Update prototype version

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
-#define VERSION_NAME_STR "Agat v1.29.2"
-#define VERSION_STR "1.29.2"
-#define VERSION_VER 1, 29, 2, 0
-#define VERSION_VER_STR "1.29.2.0"
+#define VERSION_NAME_STR "Agat v1.29.2-exp1"
+#define VERSION_STR "1.29.2.1"
+#define VERSION_VER 1, 29, 2, 1
+#define VERSION_VER_STR "1.29.2.1"
 #define VERSION_DESC_STR "Agat-7,9, Apple ][ emulator"
 #define VERSION_AUTH_STR "Nop & SnakE"
 #define VERSION_COPY_STR "Copyright © 2008-2019 Nop, SnakE"
